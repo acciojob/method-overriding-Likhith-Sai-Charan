372431208ecf10d408366b5f16cc4792ade6be9f
Likhith Sai Charan Jupudi <jupudilikhithsaicharan@gmail.com>
2025-01-18 16:53:16 +0530

    method-overriding modify 1

--- src/main/java/com/driver/A.java
+++ src/main/java/com/driver/A.java
@@ -2,7 +2,7 @@ package com.driver;
 //Task 1:
 //create a class A, with a method named meth having returning a string
 //"Invoking method from class A"
-class A {
+public class A {
     public String meth() {
         return "Invoking method from class A";
     }
